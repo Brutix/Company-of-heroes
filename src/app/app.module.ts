05e1b4452533089e8eb4435ee3191d21f505da0e
Brutix <brutix80@gmail.com>
2023-01-27 22:36:09 +0200

    last correction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TemplateComponent } from './template/template.component';
 import { ViewsComponent } from './views/views.component';
+import { ButtonComponent } from './button/button.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplateComponent,
-    ViewsComponent
+    ViewsComponent,
+    ButtonComponent,
+    HeroesComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
